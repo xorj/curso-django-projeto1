00ec8d48972bc8eac7d1a3386848cc62b747abd6
Jorge Vinícius <jorge.santos@dcomp.ufs.br>
2022-04-08 22:15:29 -0300

    Usa templates django

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context = {
+        'name': 'Jorge Vinícius'
+    })
+
+
+def sobre(request):
+    return HttpResponse("Sobre")
+
+
+def contato(request):
+    return HttpResponse("Contato")
