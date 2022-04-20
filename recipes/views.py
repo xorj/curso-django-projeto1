89cfa5cc59af260c9188a61388802f7848ed73fe
Jorge Vinícius <jorge.santos@dcomp.ufs.br>
2022-04-20 16:07:06 -0300

    Adiciona mock de receitas e listagem dinâmica

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)] 
+    })
 
 def recipe(request, id):
     print(id)
-    return render(request, 'recipes/pages/recipe-view.html')
-
+    return render(request, 'recipes/pages/recipe-view.html',  context={
+        'recipe': make_recipe()
+    })
